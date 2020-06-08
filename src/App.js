9d490f8d75c34542702c474c79c43417b24979c7
Nicolas Iniesta <inicolas69@gmail.com>
2020-06-08 21:52:22 +0200

    Amelioration Editeur

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import marked from 'marked'
+
+import { sampleText } from './sampleText'
+
+class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  componentDidMount () {
+    const text = localStorage.getItem('text')
+
+    if (text) {
+      this.setState({ text })
+    }
+  }
+
+  componentDidUpdate () {
+    const { text } = this.state
+    localStorage.setItem('text', text)
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = text => {
+    const __html = marked(text, { sanitize: true })
+    return { __html }
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <div className='container'>
+          <h1 className='title' id='title'>Editeur de Markdown</h1>
+          <div className='row'>
+            <div className='col-sm-6'>
+              <h2 className='title'>Markdown</h2>
+              <textarea
+                id='my-textarea'
+                spellcheck='false'
+                onChange={this.handleChange}
+                value={this.state.text}
+                className='form-control'
+                rows='35'
+              />
+            </div>
+            <div className='col-sm-6'>
+              <h2 className='title'>Résultat</h2>
+              <div
+                dangerouslySetInnerHTML={this.renderText(this.state.text)}
+                className='resultat'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
+
+function enableTab (id) {
+  var el = document.getElementById(id)
+  el.onkeydown = function (e) {
+    if (e.keyCode === 9) {
+      // tab was pressed
+
+      // get caret position/selection
+      var val = this.value,
+        start = this.selectionStart,
+        end = this.selectionEnd
+
+      // set textarea value to: text before caret + tab + text after caret
+      this.value = val.substring(0, start) + '\t' + val.substring(end)
+
+      // put caret at right position again
+      this.selectionStart = this.selectionEnd = start + 1
+
+      // prevent the focus lose
+      return false
+    }
+  }
+}
+
+enableTab('my-textarea')
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
